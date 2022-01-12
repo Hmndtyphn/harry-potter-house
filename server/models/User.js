5d5dec13d90a3ceeb825e2a70a743a4feabf6775
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-01-11 23:12:15 -0600

    a little more User model

--- server/models/User.js
+++ server/models/User.js
@@ -20,8 +20,23 @@ const userSchema = new Schema (
             required: true,
             minlength: 6
         },
-        wand: [Wand],
-        year: { type: Number, default: 1}
-        
+        wand: [Wand.schema],
+        year: { type: Number, default: 1},
     }
-)
+);
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
