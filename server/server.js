b8ff97fe159e1069f8b1453efb9659d15d485d98
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-11 22:25:34 -0600

    completed server, added schema/ config

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./config/connection') 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-
-});
+// const server = new ApolloServer({
+//     // typeDefs,
+//     // resolvers,
+//     // context: authMiddlerware
+// });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -19,4 +22,11 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at localhost:${PORT}${server.graphqlPath}`);
+    });
 });
