bb6d951ffea1641b69d543e4437cb6f47e9bd489
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-01-13 12:22:26 -0600

    auth.js, plus pages/Login, /Home, /Signup basic setup. Intended to devote group time to more complicated issues

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+
+const Login = (props) => {
+
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+export default Login;
