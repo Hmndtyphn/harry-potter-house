c401cb1ff6ebe772623671e2699891adfe70d882
Lori Timmons <lorietimmons@gmail.com>
2022-01-13 00:29:17 -0600

    typeDefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -3,10 +3,31 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type User {
         _id: ID
-        username: String,
-        email: String,
-        password: String,
-        house: String,
+        username: String
+        email: String
+        password: String
+        house: String
         wand: Wand
     }
+
+    type Sorting {
+        _id: ID
+        question: String
+        gryffindor: String
+        ravenclaw: String
+        hufflepuff: String 
+        slytherin: String
+
+    }
+    {
+        _id: ID
+        question: String
+        answer: String
+        subject: String 
+        professor: String
+        points: Number
+    }
+
 `
+
+module.exports = typeDefs;
