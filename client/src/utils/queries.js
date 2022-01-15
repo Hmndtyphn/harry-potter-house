0fcba10b0f7d9e8d35495a5a281a6bd7e8f5b99e
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-01-14 21:29:33 -0600

    nuking components to rebuild the way we want

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,6 +3,24 @@ import { gql } from '@apollo/client';
 export const QUERY_ALL_USERS = gql`
   {
     users {
+      _id
+      username
+      email
+      house
+      wand {
+        _id
+        core
+        wood
+        length
+      }
+    }
+  }
+`;
+
+export const QUERY_ONE_USER = gql`
+  {
+    user {
+      _id
       username
       email
       house
