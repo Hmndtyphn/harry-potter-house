933a9053c3e2862910d6fceb265bdea8e7d97891
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-01-15 09:54:30 -0600

    gql queries/mutations and renamed Home.js to GreatHall, added CommonRoom, and left notes for colleagues at start.

---
+++ client/src/pages/CommonRoom.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CommonRoom = () => {
+
+  return (
+    <section>
+      Welcome to Ravenclaw!
+      <div>Or perhaps Slytherin...</div>
+    </section>
+  )
+}
+
+export default CommonRoom;
---
+++ client/src/pages/GreatHall.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CommonRoom from './CommonRoom'
+
+const GreatHall = () => {
+  return (
+    <div>
+      <p>Good Morning Everyone!</p>
+      <CommonRoom />
+    </div>
+  )
+};
+
+export default GreatHall;
