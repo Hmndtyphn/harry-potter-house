d30815941aea8bb56c9f41b88053ddaa190c912f
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-17 16:53:06 -0600

    styling for Great hall

--- client/src/pages/GreatHall.js
+++ client/src/pages/GreatHall.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Button, Container } from '@mui/material';
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
+import { QUERY_CLASS } from "../utils/queries";
+import greatHallImage from "../assets/images/great_hall3.jpeg";
+import { Container, Typography } from "@mui/material";
+import Quiz from "../components/Quiz";
+
 
 
 // in Great Hall, link to House
@@ -12,13 +16,36 @@ import { useQuery } from "@apollo/client";
 
 
 const GreatHall = () => {
-  return (
-    <div>
-      <p>Good Morning Everyone!</p>
-      
+  // grab name from params
+  const { name } = useParams();
 
+  // use useQuery(Apollo) to make query request
+  const { loading, data } = useQuery(QUERY_CLASS, {
+    variables: { name },
+  });
+
+  const subject = data?.subject || {};
+  const { description, image, professor, questions } = subject;
+
+  const styledDiv = {
+    backgroundImage: `url(${greatHallImage})`,
+    height: "100vh",
+    color: "white",
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div style={styledDiv}>
+      <Container>
+        <Typography variant="h3" alignment="center" sx={{ p:2 }} >
+          /*/* Welcome to the Great Hall *\*\
+        </Typography>
+      </Container>
     </div>
-  )
+  );
 };
 
 export default GreatHall;
