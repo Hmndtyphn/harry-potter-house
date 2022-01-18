0e5ee4b0078a1b1e1701c1873acae465f285e059
Breania Harvey <harvey.breania@gmail.com>
2022-01-18 17:19:21 -0600

    pseudo-code added to helpers

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -3,5 +3,10 @@ export function capitalizeFirstLetter(string) {
 }
 
 // export class quiz grading function
+// take the correct answers and user answers as arguments
+// compare user answers to correct answers 
+// for each correct answer add 2 points to results array
+// for each incorrect answer subtract 1 point from results array
+// once the arrays are empty return results array
 
 // export sorting quiz grading function
