d0e268b14bac1ac2e3a589393ad7e71acf170b8b
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-01-18 23:50:08 -0600

    Change env var

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 require("dotenv").config();
 
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_KEY;
 const expiration = '2h';
 
 module.exports = {
